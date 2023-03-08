9c48812735c67a23cf278e80abb7138463880f87
DESKTOP-0M6CM8T\tj-bu-702-16 <m0m3242@gmail.com>
2023-03-08 18:04:11 +0900

    증감연산자 예제 연습

--- 19.증감연산자/js/script.js
+++ 19.증감연산자/js/script.js
@@ -55,28 +55,41 @@ $(function () {
   } 
   */
   var rdn
-  function fn1(n){
+  function fn1(n) {
     rdn = Math.floor(Math.random() * 10)
     $(`.ex3_${n} .num1`).val(rdn)
     rdn = Math.floor(Math.random() * 10)
     $(`.ex3_${n} .num2`).val(rdn)
   }
-  
+
   fn1(1)
   fn1(2)
   fn1(3)
   fn1(4)
-  
-  $(`.ex3 button`).click(function () {
-    console.log();
-    var result
-    /* if($(`.ex3_1 input:last_child`.val()) === (parseInt($(`.ex3_1 .num1`).val())+parseInt($(`.ex3_1 .num2`).val()))){
-      result = '정답'
+
+  function fn2(n) {
+    if ($(`.ex3_1 input:last-child`).val() && $(`.ex3_2 input:last-child`).val() && $(`.ex3_3 input:last-child`).val() && $(`.ex3_4 input:last-child`).val()) {
+      var 숫자1 = parseInt($(`.ex3_${n} .num1`).val())
+      var 숫자2 = parseInt($(`.ex3_${n} .num2`).val())
+      var 더하기 = 숫자1 + 숫자2
+      var 답 = parseInt($(`.ex3_${n} input:last-child`).val())
+      var 결과
+      if (n < 5 && 더하기 == 답) {
+        결과 = '정답'
+      }
+      else {
+        결과 = '오답'
+      }
+      $(`.ex3 .output`).text(결과)
     }
-    else{
-      result = '오답'
-    } */
-    $(`.ex3 .output`).text(result)
+    else {
+      alert('입력오류')
+      return false
+    }
+  }
+
+  $(`.ex3 button`).click(function () {
+    fn2()
   })
 
 })//ready
